10e59a98c18c5b4e483235cfe89668358d8ea20d
Dries Desmet <61475643+DriesDesmet1995@users.noreply.github.com>
2022-01-07 20:30:01 +0100

    Added Entities (Core)

---
+++ AIT.PE2.Socket.Core/Entities/FtFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIT.PE2.Socket.Core.Entities
+{
+    public class FTFile
+    {
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+        public byte FileSize { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public FTFile(string fileName, string fullPath, byte fileSize, DateTime creationDate)
+        {
+            FileName = fileName;
+            FullPath = fullPath;
+            FileSize = fileSize;
+            CreationDate = creationDate;
+        }
+    }
+}
---
+++ AIT.PE2.Socket.Core/Entities/FtFolder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIT.PE2.Socket.Core.Entities
+{
+    public class FTFolder
+    {
+        public string FolderName { get; set; }
+        public string FullPath { get; set; }
+        public string FullParentPath { get; set; }
+        public ICollection<FTFile> FilesInFolder { get; set; }
+
+        public FTFolder(string folderName, string fullPath, string fullParentPath, ICollection<FTFile> filesInFolder)
+        {
+            FolderName = folderName;
+            FullPath = fullPath;
+            FullParentPath = fullParentPath;
+            FilesInFolder = filesInFolder;
+        }
+    }
+}
